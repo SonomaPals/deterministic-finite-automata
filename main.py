1e623a8c01888d21bb991652097c5f800746068f
Garret Mook <mookg@sonoma.edu>
2022-03-01 16:35:22 -0800

    added grabStateNum function

--- main.py
+++ main.py
@@ -23,3 +23,27 @@ class DfaClass:
         self.prev = [1] * len(self.possibleStringList)
         self.next = [0] * len(self.possibleStringList)
         self.dictionaryCounter = dictionaryCounter
+        
+        def grabStateNum(self,inputString):
+            CharactersOfTheString = []
+            stateNum = 0
+            if (inputString == ""):
+                return 0
+            for char in inputString:
+                CharactersOfTheString.append(char)
+            for i in range(0, len(CharactersOfTheString)):
+                if CharactersOfTheString[i] == 'a':
+                    stateNum = stateNum + 1 * pow(4, (len(CharactersOfTheString) - 1) - i)
+
+                elif CharactersOfTheString[i] == 'b':
+                    stateNum = stateNum + 2 * pow(4, (len(CharactersOfTheString) - 1) - i)
+
+                elif CharactersOfTheString[i] == 'c':
+                    stateNum = stateNum + 3 * pow(4, (len(CharactersOfTheString) - 1) - i)
+
+                elif CharactersOfTheString[i] == 'd':
+                    stateNum = stateNum + 4 * pow(4, (len(CharactersOfTheString) - 1) - i)
+                else:
+                    print("ERROR: NO a,b,c, or d detected")
+                    exit(9)
+            return stateNum
